f03ecfa90164c30a3468486f152f90a577540c51
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-20 10:19:44 -0700

    Chapter 11 complete - Sam and Frodo are in Mordor and Mount Doom has been created

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -240,9 +240,13 @@ const hornOfGondor = () => {
 const itsDangerousToGoAlone = () => {
 
   // 1. take Frodo and Sam out of the fellowship and move them to Mordor (they don't need to be inside a ul in Mordor)
-
+  let $frodo = $("#the-fellowship li").eq(0);
+  $("#Mordor aside ul").append($frodo);
+  let $sam = $("#the-fellowship li").eq(0);
+  $("#Mordor aside ul").append($sam);
   // 2. add a div with an id of 'mount-doom' to Mordor
-
+  let $doom = $("<div>", {id:"mount-doom"});
+  $("#Mordor").append($doom);
 };
 
 // COMMIT YOUR WORK
